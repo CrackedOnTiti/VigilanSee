703ce29a07bc58e2db8a928db53626fe1c621d45
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-03-18 22:52:26 +0100

    [ADD] basic script that calls and dumps all contents of B2 through cargo test #15

--- vigilansee-parser/src/lib.rs
+++ vigilansee-parser/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use s3::bucket::Bucket;
+use s3::creds::Credentials;
+use s3::error::S3Error;
+use s3::region::Region;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub async fn bucket_pull() -> Result<(), S3Error> {
+    dotenvy::dotenv().ok();
+    let bucket_name = "VigilanSee-Dem";
+    let region = Region::Custom {
+    region: "eu-central-003".to_string(),
+    endpoint: "https://s3.eu-central-003.backblazeb2.com".to_string(),
+    };
+    let credentials = Credentials::default().unwrap();
+    
+    let bucket = Bucket::new(bucket_name, region, credentials)?;
+    
+    let list = bucket.list("".to_string(), None).await?;
+    
+    for dem in list {
+        println!("{:?}", dem)
     }
+    Ok(())
 }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+    
+        #[tokio::test]
+        async fn test_bucket_pull() {
+            bucket_pull().await.unwrap();
+        }
+    }
