4b822446c35688523cc9bc6b520d111072064941
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-04-23 11:03:30 +0200

    [ADD] created parse_dem.py helper script that outputs list of parsable events #19

--- scripts/parse_dem.py
+++ scripts/parse_dem.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
-import demoparser2
+from demoparser2 import DemoParser
+import sys
 
-def bruh():
-    print("yoyo")
+
+
+
+def main():
+    try:
+        arg = sys.argv[1]
+        if arg.endswith(".dem"):
+            parser = DemoParser("vigilansee-parser/dem/" + arg)
+            events = parser.list_game_events()
+            fields = parser.parse_player_info()
+            print({"events": events, "fields": fields})
+        else:
+            raise ValueError()
+            
+    except (IndexError, ValueError):
+        print("\033[0;31mExpected a \033[1;36m.dem\033[0;31m file as argument\033[0;37m")
+
+if __name__ == "__main__":
+    main()
